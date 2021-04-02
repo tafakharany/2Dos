9684a7c41cc08151528a9d209996d4819b411db7
Taha Elfakharany <taha.ashri96@outlook.com>
2021-04-02 03:11:18 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router'
 
-const routes: Routes = [];
+
+const routes: Routes =
+  [
+    { path: '', component: HomeComponent },
+    { path: 'tasks', component: HomeComponent },
+    // { path: 'tasks', component: HomeComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'tasks/:taskId', component: DetailsComponent },
+    { path: '**', component: NotFoundComponent }
+  ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { DetailsComponent } from './components/details/details.component';
+import { HeaderComponent } from './components/Shared/header/header.component';
+import { FooterComponent } from './components/Shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    HomeComponent,
+    ProfileComponent,
+    NotFoundComponent,
+    DetailsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
